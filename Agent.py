c2c7df6d5ede1aa6537a7b3c0618db01c9857d04
pumpkinhead <eylonkatz1@gmail.com>
2021-04-27 11:54:26 +0300

    still best so far

--- Agent.py
+++ Agent.py
@@ -69,7 +69,6 @@ class Agent:
 
         state = state.flatten()
         state = np.append(state, [(head.x - game.fruit.pos.x) / cell_size, (head.y - game.fruit.pos.y) / cell_size])
-        print((game.fruit.pos.x - head.x) / cell_size, (game.fruit.pos.y - head.y) / cell_size)
         return np.asarray(state)
         '''
         state = [
